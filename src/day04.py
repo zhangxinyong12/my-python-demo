c130ca3bf00b96fb130604bc8ee42a6f698c2b53
zhangxinyong <13271150671@wo.cn>
2020-03-24 00:16:01 +0800

    错误和异常

--- src/day04.py
+++ src/day04.py
@@ -1,1 +1,12 @@
-print('0000')
+#
+def add(a):
+    try:
+        print(10 + a)
+    except Exception as err:
+        print('错误信息', err)
+    finally:
+        print('最终都会执行的')
+
+
+add(23)
+add('23')
