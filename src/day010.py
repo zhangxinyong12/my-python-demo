988a163a2c68bd3257224670397ffaddf27cf788
zhangxinyong <13271150671@wo.cn>
2020-07-12 01:31:58 +0800

    no message

--- src/day010.py
+++ src/day010.py
@@ -1,17 +1,35 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.__age = age
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.__age = age
+#
+#     @property
+#     def age(self):
+#         return self.__age
+#
+#     @age.setter
+#     def age(self, age):
+#         self.__age = age
+#
+#
+# p = Person('wang', 20)
+# print(p.age)
+# # p157
 
-    @property
-    def age(self):
-        return self.__age
 
-    @age.setter
-    def age(self, age):
-        self.__age = age
+class Book:
+    def __str__(self):
+        return '11111'
 
 
-p = Person('wang', 20)
-print(p.age)
-# p157
+class Computer:
+    pass
+
+
+class Student:
+    pass
+
+
+book = Book()
+print(book)
+# p 160
