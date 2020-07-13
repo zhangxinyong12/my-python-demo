178fcb5068465ae9e688d2f199b250daf38b64ba
zhangxinyong <13271150671@wo.cn>
2020-07-13 23:47:38 +0800

    class

---
+++ src/day0713.py
@@ -0,0 +1,13 @@
+class Road:
+    def __int__(self, name, len):
+        self.name = name
+        self.len = len
+
+
+class Car:
+    def __int__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+# p161
+
+print(11111111)
