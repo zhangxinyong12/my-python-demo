bfc4a3076b347e3dd28dd7b79f456361262e9221
zhangxinyong <13271150671@wo.cn>
2020-07-14 23:45:17 +0800

    class 继承

--- src/day0713.py
+++ src/day0713.py
@@ -1,11 +1,11 @@
 class Road:
-    def __int__(self, name, len):
+    def __init__(self, name, len):
         self.name = name
         self.len = len
 
 
 class Car:
-    def __int__(self, brand, speed):
+    def __init__(self, brand, speed):
         self.brand = brand
         self.speed = speed
 # p161
